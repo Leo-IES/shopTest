255be1b8925f03ec8c1706e3344c443cd42bbe38
Leo Estrada <xxkillerxx7@gmail.com>
2020-01-27 13:43:27 -0600

    Version Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingupComponent } from './components/singup/singup.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ItemDescComponent } from './components/item-desc/item-desc.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingupComponent,
+    CartComponent,
+    ItemDescComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
