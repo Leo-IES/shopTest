4ca7de236996b9ca4f1c7469df50ac9f0ec1ef38
Leo Estrada <xxkillerxx7@gmail.com>
2020-01-27 14:02:12 -0600

    FrontEnd de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CartComponent} from './components/cart/cart.component'
+import {HomeComponent} from './components/home/home.component'
+import {ItemDescComponent}from './components/item-desc/item-desc.component'
+import {LoginComponent} from './components/login/login.component'
+import {SingupComponent} from './components/singup/singup.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'cart', component: CartComponent},
+  {path:'item-desc', component: ItemDescComponent},
+  {path:'login', component: LoginComponent},
+  {path:'singup', component: SingupComponent},
+  {path: '', pathMatch: 'full', redirectTo:'home'},
+    {path: '**', pathMatch: 'full', redirectTo:'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
