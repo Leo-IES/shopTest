b1bfe74a4bbb06dca0b3b38b773a54f79874dbf2
Leo Estrada <xxkillerxx7@gmail.com>
2020-01-29 16:14:23 -0600

    Tarjetas sin imagen

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { InfoService } from "../../services/info.service";
 
 @Component({
-  selector: 'app-item',
-  templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  selector: "app-item",
+  templateUrl: "./item.component.html",
+  styleUrls: ["./item.component.css"]
 })
 export class ItemComponent implements OnInit {
+  productos: any[ ] = [];
+  imagen: string;
 
-  item = {}
-
-  constructor(
-    private router:Router
-    ) { }
-
-  ngOnInit() {
-  }
-
-  irPaginaDelItem(){
-
-  }
-
-  agregarAlCarrito(){
+  constructor(private router: Router, private products: InfoService) {
+    this.productos= this.products.products;
+    this.imagen = this.products.products[0].images[0].url
+    console.log(this.productos)
     
   }
+
+  ngOnInit() {
+    
+  }
+
+  // irPaginaDelItem() {}
+
+  // agregarAlCarrito() {}
 }
